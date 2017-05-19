ca1ba5035c625767d910fb956dc25ac6c832290f
Matthew Savage <matt@amasses.net>
2017-05-19 11:14:59 +1000

    Added apply return listener examples

---
+++ apply-return-listener.cs
@@ -0,0 +1,20 @@
+using System;
+using EncryptLibrary;
+using System.Collections.Generic;
+using System.IO;
+
+String output = Request.Querystring["eapps_merchantData"];
+// Store the hopData paramater in a session variable to use later
+String encrypted_hopData_param = Request.Querystring["eapps_hopData"];
+
+String keyFilePath = "/path/to/file/keyfile.key";
+
+IDictionary outputDecrypted = null;
+try {
+   outputDecrypted = ResponseProcessor.generateResponse(output, keyFile);
+   foreach(KeyValuePair entry in outputDecrypted) {
+		Console.WriteLine("entry.key= " + entry.Key + " and entry.value= " + entry.Value);
+   }
+} catch(Exception ex) {
+   Console.WriteLine("Exception is=" + ex.StackTrace);
+}
