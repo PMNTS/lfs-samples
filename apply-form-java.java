a59ac5e6bf1ebc42e2765a0899fcba4f5da8188c
Matthew Savage <matt@amasses.net>
2017-05-19 11:01:17 +1000

    Added samples for CS and Java apply form

---
+++ apply-form-java.java
@@ -0,0 +1,9 @@
+url = "https://onlinepaymentintegration.gemoney.com.au/eapps/ApplyRetail.faces";
+out.print("<html>\n");
+out.print("<form name=\"merchantForm\" action=\"" + url + "\" method=\"POST\">\n");
+out.print("<input type=\"hidden\" name=\"merchantID\" value=\"" + output.get(MerchantRequestParameter.MERCHANT_ID) + "\">\n");
+out.print("<input type=\"hidden\" name=\"source\" value=\"" + output.get(MerchantRequestParameter.SOURCE_FLAG) + "\">\n");
+out.print("<input type=\"hidden\" name=\"merchant_eappsData\" value=\"" + output.get(MerchantRequestParameter.IN_DATA_BLOCK) + "\">\n");
+out.print("<input type=\"submit\" name=\"Submit\" value=\"Submit\">\n");
+out.print("</form>");
+out.print("</html>");
